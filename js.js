6949218c43930980e2435454310b85760e206324
Toph Tucker <tophtucker@gmail.com>
2014-11-07 22:20:46 -0500

    2

--- js.js
+++ js.js
@@ -1,3 +1,18 @@
+var ᴨ = Math.PI,
+    ξ = Math.random;
+
 document.addEventListener("DOMContentLoaded", function() {
-  var div = document.querySelector("div");
+  var 〇 = document.querySelector("div"),
+      w = window.innerWidth,
+      h = window.innerHeight;
+
+  var x = Math.round(w * ξ()),
+      y = Math.round(h * ξ());
+
+  var ẋ = Math.round(w * ξ()),
+      ẏ = Math.round(h * ξ());
+
+  var θ = Math.round(2 * ᴨ * ξ());
+
+  〇.setAttribute("style","transform: translate(-50%,-50%) rotate("+θ+"rad); left: "+x+"px; top: "+y+"px; width: "+ẋ+"px; height: "+ẏ+"px;");
 });
